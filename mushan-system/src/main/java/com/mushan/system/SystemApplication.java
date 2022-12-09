ac6da581a4ddd2dbc80bc3dc3abe19ed2ea7c1dd
木杉 <lilinlin>
2022-12-09 17:08:47 +0800

    into

---
+++ mushan-system/src/main/java/com/mushan/system/SystemApplication.java
@@ -0,0 +1,16 @@
+package com.mushan.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableDiscoveryClient
+public class SystemApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SystemApplication.class, args);
+    }
+}
