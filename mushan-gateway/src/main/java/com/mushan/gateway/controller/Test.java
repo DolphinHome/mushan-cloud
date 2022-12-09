c06736d57889a4e609026b23f536ec8d93b82868
木杉 <lilinlin>
2022-12-09 16:33:18 +0800

    into

--- mushan-gateway/src/main/java/com/mushan/gateway/controller/Test.java
+++ mushan-gateway/src/main/java/com/mushan/gateway/controller/Test.java
@@ -1,5 +1,6 @@
 package com.mushan.gateway.controller;
 
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Test {
 
-    @Value("aa")
+    @Value("${wps.secret}")
     private String aa;
 
     @GetMapping("/t1")
