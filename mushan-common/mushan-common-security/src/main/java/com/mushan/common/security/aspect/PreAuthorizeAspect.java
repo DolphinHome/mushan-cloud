2778577efe60b3e330f1caf23be6f3e0c9af2fda
木杉 <lilinlin>
2022-12-09 14:17:25 +0800

    into

---
+++ mushan-common/mushan-common-security/src/main/java/com/mushan/common/security/aspect/PreAuthorizeAspect.java
@@ -0,0 +1,51 @@
+package com.mushan.common.security.aspect;
+
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+@Aspect
+public class PreAuthorizeAspect {
+    /**
+     * 构建
+     */
+    public PreAuthorizeAspect() {
+    }
+    /**
+     * 定义AOP签名 (切入所有使用鉴权注解的方法)
+     */
+    public static final String POINTCUT_SIGN = " @annotation(com.mushan.common.security.annotation.RequiresPermissions)";
+
+    /**
+     * 声明AOP签名
+     */
+    @Pointcut(POINTCUT_SIGN)
+    public void pointcut() {
+    }
+
+    /**
+     * 环绕切入
+     *
+     * @param joinPoint 切面对象
+     * @return 底层方法执行后的返回值
+     * @throws Throwable 底层方法抛出的异常
+     */
+    @Around("pointcut()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        // 注解鉴权
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        try {
+            // 执行原有逻辑
+            Object obj = joinPoint.proceed();
+            return obj;
+        } catch (Throwable e) {
+            throw e;
+        }
+    }
+
+}
